b102483a4c0103f724f6fadce59fd562b3ff313c
vikasvelagapudi <vvelagapudi0717@gmail.com>
2014-03-27 11:11:20 -0400

    Add displaying of dataset sample

--- static/js/script.js
+++ static/js/script.js
@@ -11,37 +11,40 @@ app.config(function($interpolateProvider) {
 });
 
 app.controller('DatasetListCtrl', function($scope, $http) {
-  window.onload = function() {
-    var files;
+  var files;
 
-    $('#data-file').on('change', function(event) {
-      files = event.target.files;
+  $('#data-file').on('change', function(event) {
+    files = event.target.files;
+  });
+
+  $('#data-submit').click(function(event) {
+    var data = new FormData();
+    data.append('dataset', files[0])
+
+    $.ajax({
+      url: '/upload',
+      type: 'POST',
+      data: data,
+      cache: false,
+      processData: false,
+      contentType: false,
+    }).success(function(data) {
+      if (data.status === "success") {
+        delete data['status'];
+
+        // update model with file data
+        $scope.$apply(function() {
+          data.title = data.filename;
+          $scope.datasets.push(data);
+        });
+      }
     });
+  });
 
-    $('#data-submit').click(function(event) {
-      var data = new FormData();
-      data.append('dataset', files[0])
-
-      $.ajax({
-        url: '/upload',
-        type: 'POST',
-        data: data,
-        cache: false,
-        processData: false,
-        contentType: false,
-      }).success(function(data) {
-        if (data.status === "success") {
-          delete data['status'];
-
-          // update model with file data
-          $scope.$apply(function() {
-            data.title = data.filename;
-            $scope.datasets.push(data);
-          });
-        }
-      });
-    });
-  };
+  $scope.select_dataset = function(index) {
+    $scope.selected_index = index;
+    console.log(index);
+  }
 
   $scope.datasets = [
     {
@@ -54,7 +57,20 @@ app.controller('DatasetListCtrl', function($scope, $http) {
       rows: 150,
       cols: 1500,
       type: "TSV"
-    }
+    }, {
+      cols:3,
+      filename:"student.csv",
+      rows:6,
+      sample:{
+        "0":["vikas","student","mit"],
+        "1":["kevin","graduate","mit"],
+        "2":["alyssa","student","mit"],
+        "3":["ben","student","mit"],
+        "4":["alice","graduate","mit"],
+        "5":["bob","graduate","mit"] },
+      type:"csv",
+      title:"student.csv"
+    },
   ];
 
   return console.log($scope);
