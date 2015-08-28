b8f5c4d29ad26f7d3f0efb5f79bf4dbdac16d772
kevinzenghu <kzh@mit.edu>
2015-08-28 11:49:44 -0400

    Actually returning specs in endpoint now (un-scored)

--- server/visualization/viz_specs.py
+++ server/visualization/viz_specs.py
@@ -44,33 +44,38 @@ elementwise_functions = {
     'divide': '/'
 }
 
-def A(label):
+###
+# Functions providing only the new specs for each case (subsumed cases are taken care of elsewhere)
+###
+def A(q_field):
     specs = []
+
+    q_label = q_field['label']
 
     # { Index: value }
     index_spec = {
         'structure': 'ind:val',
         'args': {
-            'field_a': label
+            'field_a': q_label
         },
         'meta': {
-            'desc': 'Plot %s against its index' % label
+            'desc': 'Plot %s against its index' % (q_label)
         }
     }
     specs.append(index_spec)
 
-    # TODO Make this depend on non-unique values
-    # { Value: count }
-    count_spec = {
-        'structure': 'val:count',
-        'args': {
-            'field_a': label
-        },
-        'meta': {
-            'desc': 'Plot values of %s against count of occurrences' % label
+    if not q_field['unique']:
+        # { Value: count }
+        count_spec = {
+            'structure': 'val:count',
+            'args': {
+                'field_a': q_label
+            },
+            'meta': {
+                'desc': 'Plot values of %s against count of occurrences' % q_label
+            }
         }
-    }
-    specs.append(count_spec)
+        specs.append(count_spec)
 
     # TODO Implement binning algorithm
     # { Bins: Aggregate(binned values) }
@@ -79,67 +84,68 @@ def A(label):
             'structure': 'bin:agg',
             'args': {
                 'agg_fn': agg_fn,
-                'agg_field_a': label,
+                'agg_field_a': q_label,
                 'binning_spec': 0,
-                'binning_field': label
+                'binning_field': q_label
             },
             'meta': {
-                'desc': 'Bin %s, then aggregate binned values by %s' % (label, agg_fn)
+                'desc': 'Bin %s, then aggregate binned values by %s' % (q_label, agg_fn)
             }
         }
         specs.append(bin_spec)
     return specs
 
-def B(labels):
+def B(q_fields):
     specs = []
+
     # Function on pairs of columns
-    for (field_a, field_b) in combinations(labels, 2):
+    for (field_a, field_b) in combinations(q_fields, 2):
+        label_a = field_a['label']
+        label_b = field_b['label']
         for ew_fn, ew_op in elementwise_functions.iteritems():
-            derived_column_desc = "%s %s %s" % (field_a, ew_op, field_b)
-            A_specs = A(derived_column_desc)
+            derived_column_field = {
+                'transform': '2:1',
+                'label': "%s %s %s" % (label_a, ew_op, label_b),
+                'unique': False  # TODO Run property detection again?
+            }
+            A_specs = A(derived_column_field)
             specs.extend(A_specs)
     return specs
 
-def C(label):
+def C(c_field):
     specs = []
+    c_label = c_field['label']
 
     # TODO Only create if values are non-unique
     spec = {
         'structure': 'val:count',
         'args': {
-            'field_a': label
+            'field_a': c_label
         },
         'meta': {
-            'desc': 'Unique values of %s mapped to number of occurrences' % (label)
+            'desc': 'Unique values of %s mapped to number of occurrences' % (c_label)
         }
     }
+    specs.append(spec)
     return specs
 
-def D(c_label, q_label):
+def D(c_field, q_field):
     specs = []
-    c_unique = false
+    c_label = c_field['label']
+    q_label = q_field['label']
 
-    # One case of A
-    a_spec = A(c_label)
-    specs.append(a_spec)
-
-    # One case of C
-    c_spec = C(q_label)
-    specs.append(q_label)
-
-    # If c_label is unique
-    if c_unique:
+    if c_field['unique']:
         spec = {
             'structure': 'val:val',
             'args': {
                 'field_a': c_label,
-                'field_b': q_label
+                'field_b': q_label,
             },
             'meta': {
                 'desc': 'Plotting raw values of %s against corresponding values of %s' % (c_label, q_label)
             }
         }
-        spec.append(spec)
+        specs.append(spec)
     else:
         for agg_fn in aggregation_functions.keys():
             spec = {
@@ -156,6 +162,89 @@ def D(c_label, q_label):
             specs.append(spec)
     return specs
 
+def E(c_field, q_fields):
+    specs = []
+
+    # Two-field agg:agg
+    if not c_field['unique']:
+        c_label = c_field['label']
+        for (q_field_a, q_field_b) in combinations(q_fields, 2):
+            q_label_a, q_label_b = q_field_a['label'], q_field_b['label']
+            for agg_fn in aggregation_functions.keys():
+                spec = {
+                    'structure': 'agg:agg',
+                    'args': {
+                        'agg_fn': agg_fn,
+                        'agg_field_a': q_label_a,
+                        'agg_field_b': q_label_b,
+                        'grouped_field': c_label
+                    },
+                    'meta': {
+                        'desc': 'Plotting aggregated values of %s against aggregated values of %s, grouped by %s' % (q_label_a, q_label_b, c_label)
+                    }
+                }
+    return specs
+
+def F(c_fields):
+    specs = []
+
+    # Two-field val:val
+    for (c_field_a, c_field_b) in combinations(c_fields, 2):
+        c_label_a, c_label_b = c_field_a['label'], c_field_b['label']
+        spec = {
+            'structure': 'val:val',
+            'args': {
+                'field_a': c_label_a,
+                'field_b': c_label_b
+            },
+            'meta': {
+                'desc': 'Plotting values of %s against corresponding values of %s' % (c_label_a, c_label_b)
+            }
+        }
+        specs.append(spec)
+    return specs
+
+def G(c_fields, q_field):
+    specs = []
+    # TODO How do you deal with this?
+    # Two-field val:val:q with quantitative data
+    for (c_field_a, c_field_b) in combinations(c_fields, 2):
+        c_label_a, c_label_b = c_field_a['label'], c_field_b['label']
+        q_label = q_field['label']
+        spec = {
+            'structure': 'val:val:q',
+            'args': {
+                'field_a': c_label_a,
+                'field_b': c_label_b,
+                'data_field_a': q_label
+            },
+            'meta': {
+                'desc': 'Plotting values of %s against corresponding values of %s with attached data field %s' % (c_label_a, c_label_b, q_label)
+            }
+        }
+        specs.append(spec)
+    return specs
+
+def H(c_fields, q_fields):
+    specs = []
+    # TODO How do you deal with this?
+    # Two-field val:val:[q] with quantitative data
+    for (c_field_a, c_field_b) in combinations(c_fields, 2):
+        c_label_a, c_label_b = c_field_a['label'], c_field_b['label']
+        q_labels = [ f['label'] for f in q_fields ]
+        spec = {
+            'structure': 'val:val:q',
+            'args': {
+                'field_a': c_label_a,
+                'field_b': c_label_b,
+                'data_fields': q_labels
+            },
+            'meta': {
+                'desc': 'Plotting values of %s against corresponding values of %s with attached data fields %s' % (c_label_a, c_label_b, q_labels)
+            }
+        }
+        specs.append(spec)
+    return specs
 
 # TODO Move the case classifying into dataset ingestion (doesn't need to be here!)
 # 1) Enumerated viz specs given data, properties, and ontologies
@@ -164,33 +253,40 @@ def enumerate_viz_specs(datasets, properties, ontologies):
     specs_by_dID = dict([(dID, []) for dID in dIDs])
 
     types_by_dID = {}
-    properties_by_dID = {}
-    labels_by_dID = {}
+    fields_by_dID = {}
+
     for p in properties:
         dID = p['dID']
-        if dID in properties_by_dID and dID in types_by_dID:
+        relevant_fields = {
+            'label': p['label'],
+            'unique': p['unique'],
+            'type': p['type'],
+            'normality': p['normality'],
+            'values': p['values']
+        }
+        if dID in fields_by_dID:
             # TODO Necessary to preserve the order of fields?
-            types_by_dID[dID].append(p['type'])
-            labels_by_dID[dID].append(p['label'])
-            properties_by_dID[dID].append(p)
+            fields_by_dID[dID].append(relevant_fields)
         else:
-            types_by_dID[dID] = [ p['type'] ]
-            labels_by_dID[dID] = [ p['label'] ]
-            properties_by_dID[dID] = [ p ]
+            fields_by_dID[dID] = [ relevant_fields ]
 
     # Iterate through datasets (no cross-dataset visualizations for now)
-    for dID in properties_by_dID.keys():
+    for dID in fields_by_dID.keys():
         specs = []
-        specific_types = types_by_dID[dID]
-        labels = labels_by_dID[dID]
-        types = [ specific_to_general_type[t] for t in specific_types ]
-        properties = properties_by_dID[dID]
 
-        q_count = types.count('q')
-        c_count = types.count('c')
+        c_fields = []
+        q_fields = []
+        fields = fields_by_dID[dID]
+        for f in fields:
+            general_type = specific_to_general_type[f['type']]
+            if general_type is 'q':
+                q_fields.append(f)
+            elif general_type is 'c':
+                c_fields.append(f)
 
-        print "\tN_q:", q_count
-        print "\tN_c:", c_count
+        c_count = len(c_fields)
+        q_count = len(q_fields)
+
 
         # Cases A - B
         # Q > 0, C = 0
@@ -199,59 +295,146 @@ def enumerate_viz_specs(datasets, properties, ontologies):
             # Case A) Q = 1, C = 0
             if q_count == 1:
                 print "Case A"
-                label = labels[0]
-                A_specs = A(label)
+                q_field = q_fields[0]
+                A_specs = A(q_field)
                 specs.extend(A_specs)
             elif q_count >= 1:
                 print "Case B"
-                for label in labels:
-                    A_specs = A(label)
+                for q_field in q_fields:
+                    A_specs = A(q_field)
                     specs.extend(A_specs)
-                B_specs = B(labels)
+                B_specs = B(q_fields)
                 specs.extend(B_specs)
 
         # Cases C - E
         # C = 1
-        if c_count == 1:
+        elif c_count == 1:
             # Case C) C = 1, Q = 0
             if q_count == 0:
                 print "Case C"
-                continue
+                c_field = c_fields[0]
+                C_specs = C(c_field)
+                specs.extend(C_specs)
+
             # Case D) C = 1, Q = 1
             elif q_count == 1:
                 print "Case D"
-                continue
+
+                # One case of A
+                q_field = q_fields[0]
+                A_specs = A(q_field)
+                specs.extend(A_specs)
+
+                # One case of C
+                c_field = c_fields[0]
+                C_specs = C(c_field)
+                specs.extend(C_specs)
+
+                # One case of D
+                c_field, q_field = c_fields[0], q_fields[0]
+                D_specs = D(c_field, q_field)
+                specs.extend(D_specs)
+
             # Case E) C = 1, Q >= 1
             elif q_count > 1:
                 print "Case E"
-                continue
+
+                # N_Q cases of A
+                for q_field in q_fields:
+                    A_specs = A(q_field)
+                    specs.extend(A_specs)
+
+                # N_C cases of C
+                for c_field in c_fields:
+                    C_specs = C(c_field)
+                    specs.extend(C_specs)
+
+                # One case of B
+                B_specs = B(q_fields)
+                specs.extend(B_specs)
+
+                # One case of E
+                E_specs = E(c_field, q_fields)
+                specs.extend(E_specs)
 
         # Cases F - H
         # C >= 1
-        if c_count >= 1:
+        elif c_count >= 1:
             # Case F) C >= 1, Q = 0
             if q_count == 0:
                 print "Case F"
-                continue
+
+                # N_C cases of C
+                for c_field in c_fields:
+                    C_specs = C(c_field)
+                    specs.extend(C_specs)
+
+                # One case of F
+                F_specs = F(c_fields)
+                specs.extend(F_specs)
+
             # Case G) C >= 1, Q = 1
             elif q_count == 1:
                 print "Case G"
-                continue
+                q_field = q_fields[0]
+
+                # N_C cases of D
+                for c_field in c_fields:
+                    D_specs = D(c_field, q_field)
+                    specs.extend(D_specs)
+
+                # One case of F
+                F_specs = F(c_fields)
+                specs.extend(F_specs)
+
+                # One case of G
+                G_specs = G(c_fields, q_field)
+                specs.extend(G_specs)
+
             # Case H) C >= 1, Q > 1
             elif q_count > 1:
                 print "Case H"
-                continue
+
+                # N_C cases of C
+                # N_C cases of E
+                for c_field in c_fields:
+                    C_specs = C(c_field)
+                    specs.extend(C_specs)
+
+                    E_specs = E(c_field, q_fields)
+                    specs.extend(E_specs)
+
+
+                # N_Q cases of A
+                # N_Q cases of G
+                for q_field in q_fields:
+                    A_specs = A(q_field)
+                    specs.extend(A_specs)
+
+                    G_specs = G(c_fields, q_field)
+                    specs.extend(G_specs)
+
+                # One case of B
+                B_specs = B(q_fields)
+                specs.extend(B_specs)
+
+                # One case of F
+                F_specs = F(c_fields)
+                specs.extend(F_specs)
+
         specs_by_dID[dID] = specs
 
-    print "Specs:", len(specs_by_dID)
-    pprint(specs_by_dID)
+        print "\tN_c:", c_count
+        print "\tN_q:", q_count
+        print "\tNumber of specs:", len(specs_by_dID[dID])
+        # pprint(specs_by_dID)
     return specs_by_dID
 # 2) Filtering enumerated viz specs based on interpretability and renderability
 def filter_viz_specs(enumerated_viz_specs):
-    filtered_viz_specs = []
+    filtered_viz_specs = enumerated_viz_specs
     return filtered_viz_specs
 
 # 3) Scoring viz specs based on effectiveness, expressiveness, and statistical properties
 def score_viz_specs(filtered_viz_specs):
-    scored_viz_specs = []
+    scored_viz_specs = filtered_viz_specs
     return scored_viz_specs
